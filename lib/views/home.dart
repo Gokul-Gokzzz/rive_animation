b4cf249e9bedccb1f888ce85fdb0b0eae303e76a
Gokul-Gokzzz <gokulunniu88@gmail.com>
2024-04-25 17:02:54 +0530

    adding dialog box

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,13 @@
 import 'dart:ui';
 
+import 'package:animation/widgets/btn.dart';
+import 'package:animation/widgets/sign_in_form.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rive/rive.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,24 +18,217 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late RiveAnimationController btnAnimationController;
+  @override
+  void initState() {
+    btnAnimationController = OneShotAnimation(
+      "active",
+      autoplay: false,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          RiveAnimation.asset('assets/RiveAssets/shapes.riv'),
+          Positioned(
+            width: MediaQuery.of(context).size.width * 1.7,
+            bottom: 200,
+            left: 100,
+            child: Image.asset('assets/Backgrounds/Spline.png'),
+          ),
           Positioned.fill(
             child: BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
-              child: SizedBox(),
+              child: const SizedBox(),
             ),
           ),
-          Positioned(
-              width: MediaQuery.of(context).size.width * 1.7,
-              bottom: 200,
-              left: 100,
-              child: Image.asset('assets/Backgrounds/Spline.png'))
+          const RiveAnimation.asset('assets/RiveAssets/shapes.riv'),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              child: const SizedBox(),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(),
+                  const SizedBox(
+                    width: 260,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Learn design & code',
+                          style: TextStyle(
+                              fontSize: 60, fontFamily: "Poppins", height: 1.2),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                            "Don't skip the design.Learn design and code , by building real apps with flutter and swift . Complete course about the best tool "),
+                      ],
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 2,
+                  ),
+                  BtnAnimation(
+                    btnAnimationController: btnAnimationController,
+                    press: () {
+                      btnAnimationController.isActive = true;
+                      Future.delayed(
+                        Duration(milliseconds: 800),
+                        () {
+                          customSinginDialog(context);
+                        },
+                      );
+                    },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 24),
+                    child: Text(
+                      "Purchase includes acess to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificate.",
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
+      ),
+    );
+  }
+
+  Future<Object?> customSinginDialog(BuildContext context) {
+    return showGeneralDialog(
+      barrierDismissible: true,
+      barrierLabel: "Sign In",
+      context: context,
+      transitionDuration: Duration(milliseconds: 400),
+      transitionBuilder: (_, animation, __, child) {
+        Tween<Offset> tween;
+        tween = Tween(begin: Offset(0, -1), end: Offset.zero);
+        return SlideTransition(
+          position: tween.animate(
+            CurvedAnimation(parent: animation, curve: Curves.easeInOut),
+          ),
+          child: child,
+        );
+      },
+      pageBuilder: (context, _, __) => Center(
+        child: Container(
+          height: 620,
+          margin: const EdgeInsets.symmetric(horizontal: 16),
+          padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+          decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.94),
+              borderRadius: const BorderRadius.all(Radius.circular(40))),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Stack(clipBehavior: Clip.none, children: [
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const Text(
+                      "Sign in",
+                      style: TextStyle(
+                        fontSize: 34,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      child: Text(
+                        "Access to 240+ hours of content. Learn design and code, by building real apps with Flutter and Swift.",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SignInForm(),
+                    const Row(
+                      children: [
+                        Expanded(
+                          child: Divider(),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16),
+                          child: Text(
+                            "OR",
+                            style: TextStyle(
+                              color: Colors.black26,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        Expanded(child: Divider()),
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 24),
+                      child: Text(
+                        "Sign up with Email, Apple or Google",
+                        style: TextStyle(color: Colors.black54),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          padding: EdgeInsets.zero,
+                          icon: SvgPicture.asset(
+                            "assets/icons/email_box.svg",
+                            height: 64,
+                            width: 64,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          padding: EdgeInsets.zero,
+                          icon: SvgPicture.asset(
+                            "assets/icons/apple_box.svg",
+                            height: 64,
+                            width: 64,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          padding: EdgeInsets.zero,
+                          icon: SvgPicture.asset(
+                            "assets/icons/google_box.svg",
+                            height: 64,
+                            width: 64,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const Positioned(
+                left: 0,
+                right: 0,
+                bottom: -48,
+                child: CircleAvatar(
+                  radius: 16,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.close,
+                    size: 20,
+                    color: Colors.black,
+                  ),
+                ),
+              )
+            ]),
+          ),
+        ),
       ),
     );
   }
