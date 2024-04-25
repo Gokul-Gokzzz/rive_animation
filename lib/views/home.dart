d405ad2d53fc18db57ab9591c342b27bb5484bf6
Gokul-Gokzzz <gokulunniu88@gmail.com>
2024-04-26 00:06:09 +0530

    adding new features

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,13 +1,13 @@
 import 'dart:ui';
 
 import 'package:animation/widgets/btn.dart';
-import 'package:animation/widgets/sign_in_form.dart';
+import 'package:animation/widgets/custom_dialog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
 import 'package:rive/rive.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -18,6 +18,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isSigninDialog = false;
   late RiveAnimationController btnAnimationController;
   @override
   void initState() {
@@ -52,183 +53,71 @@ class _HomeScreenState extends State<HomeScreen> {
               child: const SizedBox(),
             ),
           ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Spacer(),
-                  const SizedBox(
-                    width: 260,
-                    child: Column(
-                      children: [
-                        Text(
-                          'Learn design & code',
-                          style: TextStyle(
-                              fontSize: 60, fontFamily: "Poppins", height: 1.2),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Text(
-                            "Don't skip the design.Learn design and code , by building real apps with flutter and swift . Complete course about the best tool "),
-                      ],
-                    ),
-                  ),
-                  const Spacer(
-                    flex: 2,
-                  ),
-                  BtnAnimation(
-                    btnAnimationController: btnAnimationController,
-                    press: () {
-                      btnAnimationController.isActive = true;
-                      Future.delayed(
-                        Duration(milliseconds: 800),
-                        () {
-                          customSinginDialog(context);
-                        },
-                      );
-                    },
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 24),
-                    child: Text(
-                      "Purchase includes acess to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificate.",
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Future<Object?> customSinginDialog(BuildContext context) {
-    return showGeneralDialog(
-      barrierDismissible: true,
-      barrierLabel: "Sign In",
-      context: context,
-      transitionDuration: Duration(milliseconds: 400),
-      transitionBuilder: (_, animation, __, child) {
-        Tween<Offset> tween;
-        tween = Tween(begin: Offset(0, -1), end: Offset.zero);
-        return SlideTransition(
-          position: tween.animate(
-            CurvedAnimation(parent: animation, curve: Curves.easeInOut),
-          ),
-          child: child,
-        );
-      },
-      pageBuilder: (context, _, __) => Center(
-        child: Container(
-          height: 620,
-          margin: const EdgeInsets.symmetric(horizontal: 16),
-          padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
-          decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.94),
-              borderRadius: const BorderRadius.all(Radius.circular(40))),
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: Stack(clipBehavior: Clip.none, children: [
-              SingleChildScrollView(
+          AnimatedPositioned(
+            top: isSigninDialog ? -50 : 0,
+            duration: Duration(milliseconds: 240),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text(
-                      "Sign in",
-                      style: TextStyle(
-                        fontSize: 34,
-                        fontFamily: "Poppins",
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                      child: Text(
-                        "Access to 240+ hours of content. Learn design and code, by building real apps with Flutter and Swift.",
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    const SignInForm(),
-                    const Row(
-                      children: [
-                        Expanded(
-                          child: Divider(),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          child: Text(
-                            "OR",
+                    const Spacer(),
+                    const SizedBox(
+                      width: 260,
+                      child: Column(
+                        children: [
+                          Text(
+                            'Learn design & code',
                             style: TextStyle(
-                              color: Colors.black26,
-                              fontWeight: FontWeight.w500,
-                            ),
+                                fontSize: 60,
+                                fontFamily: "Poppins",
+                                height: 1.2),
                           ),
-                        ),
-                        Expanded(child: Divider()),
-                      ],
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                              "Don't skip the design.Learn design and code , by building real apps with flutter and swift . Complete course about the best tool "),
+                        ],
+                      ),
+                    ),
+                    const Spacer(
+                      flex: 2,
+                    ),
+                    BtnAnimation(
+                      btnAnimationController: btnAnimationController,
+                      press: () {
+                        btnAnimationController.isActive = true;
+                        Future.delayed(
+                          Duration(milliseconds: 800),
+                          () {
+                            setState(() {
+                              isSigninDialog = true;
+                            });
+                            customSinginDialog(context, onClosed: (_) {
+                              setState(() {
+                                isSigninDialog = false;
+                              });
+                            });
+                          },
+                        );
+                      },
                     ),
                     const Padding(
                       padding: EdgeInsets.symmetric(vertical: 24),
                       child: Text(
-                        "Sign up with Email, Apple or Google",
-                        style: TextStyle(color: Colors.black54),
+                        "Purchase includes acess to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificate.",
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          padding: EdgeInsets.zero,
-                          icon: SvgPicture.asset(
-                            "assets/icons/email_box.svg",
-                            height: 64,
-                            width: 64,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          padding: EdgeInsets.zero,
-                          icon: SvgPicture.asset(
-                            "assets/icons/apple_box.svg",
-                            height: 64,
-                            width: 64,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          padding: EdgeInsets.zero,
-                          icon: SvgPicture.asset(
-                            "assets/icons/google_box.svg",
-                            height: 64,
-                            width: 64,
-                          ),
-                        ),
-                      ],
-                    ),
+                    )
                   ],
                 ),
               ),
-              const Positioned(
-                left: 0,
-                right: 0,
-                bottom: -48,
-                child: CircleAvatar(
-                  radius: 16,
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                    Icons.close,
-                    size: 20,
-                    color: Colors.black,
-                  ),
-                ),
-              )
-            ]),
-          ),
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/widgets/sign_in_form.dart
+++ lib/widgets/sign_in_form.dart
@@ -1,71 +1,119 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:rive/rive.dart';
 
-class SignInForm extends StatelessWidget {
+class SignInForm extends StatefulWidget {
   const SignInForm({
     super.key,
   });
 
   @override
+  State<SignInForm> createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool isShowLoaded = false;
+  late SMITrigger check;
+  late SMITrigger error;
+  late SMITrigger reset;
+  StateMachineController getRiveController(Artboard artboard) {
+    StateMachineController? controller =
+        StateMachineController.fromArtboard(artboard, "State Machine 1");
+    artboard.addController(controller!);
+    return controller;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Form(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Stack(
       children: [
-        const Text(
-          'Email',
-          style: TextStyle(color: Colors.black54),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 8, bottom: 16),
-          child: TextFormField(
-            decoration: InputDecoration(
-              prefixIcon: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: SvgPicture.asset("assets/icons/email.svg"),
+        Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Email',
+                style: TextStyle(color: Colors.black54),
               ),
-            ),
-          ),
-        ),
-        const Text(
-          'Password',
-          style: TextStyle(color: Colors.black54),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 8, bottom: 16),
-          child: TextFormField(
-            obscureText: true,
-            decoration: InputDecoration(
-              prefixIcon: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: SvgPicture.asset("assets/icons/password.svg"),
+              Padding(
+                padding: const EdgeInsets.only(top: 8, bottom: 16),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "";
+                    }
+                    return null;
+                  },
+                  onSaved: (email) {},
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: SvgPicture.asset("assets/icons/email.svg"),
+                    ),
+                  ),
+                ),
               ),
-            ),
+              const Text(
+                'Password',
+                style: TextStyle(color: Colors.black54),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8, bottom: 16),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: SvgPicture.asset("assets/icons/password.svg"),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8, bottom: 24),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                    } else {}
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFF77D8E),
+                      minimumSize: const Size(double.infinity, 56),
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(25),
+                        bottomRight: Radius.circular(25),
+                        bottomLeft: Radius.circular(25),
+                      ))),
+                  icon: const Icon(
+                    CupertinoIcons.arrow_right,
+                    color: Color(0xFFFE0037),
+                  ),
+                  label: const Text('Sign In'),
+                ),
+              )
+            ],
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(top: 8, bottom: 24),
-          child: ElevatedButton.icon(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFFF77D8E),
-                minimumSize: const Size(double.infinity, 56),
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(25),
-                  bottomRight: Radius.circular(25),
-                  bottomLeft: Radius.circular(25),
-                ))),
-            icon: const Icon(
-              CupertinoIcons.arrow_right,
-              color: Color(0xFFFE0037),
-            ),
-            label: const Text('Sign In'),
-          ),
-        )
+        isShowLoaded
+            ? Positioned.fill(
+                child: RiveAnimation.asset(
+                  'assets/RiveAssets/check.riv',
+                  onInit: (artboard) {
+                    StateMachineController controller =
+                        getRiveController(artboard);
+                    check = controller.findSMI("Check") as SMITrigger;
+                    error = controller.findSMI("Error") as SMITrigger;
+                    reset = controller.findSMI("Reset") as SMITrigger;
+                  },
+                ),
+              )
+            : SizedBox()
       ],
-    ));
+    );
   }
 }
